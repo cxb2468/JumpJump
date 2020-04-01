b630d251b870a5a87fa68dc30f78cef1630657ff
cxb2468 <cxb2468@qq.com>
2020-04-01 22:45:23 +0800

    github test!

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+    private   int   i;
+    private  boolean  st;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public boolean isSt() {
+        return st;
+    }
+
+    public void setSt(boolean st) {
+        this.st = st;
+    }
+}
