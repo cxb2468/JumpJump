95ec4b4a2f2e7da3de019555735210930274a5b9
cxb2468 <cxb2468@qq.com>
2020-04-03 13:53:15 +0800

    //0403 恐龙类 更新完毕

--- src/KLong.java
+++ src/KLong.java
@@ -1,4 +1,8 @@
+import com.sun.xml.internal.org.jvnet.fastinfoset.stax.LowLevelFastInfosetStreamWriter;
+import org.w3c.dom.css.Rect;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -16,6 +20,7 @@ public class KLong {
     final int LOWEST_Y = 200;
     int jumpValue = 5;
 
+    //恐龙方法
     public KLong(){
         try{
             image1 = ImageIO.read(new File("image/long1.png"));
@@ -25,11 +30,54 @@ public class KLong {
         }catch (Exception e){
                e.printStackTrace();
         }
+        x = 50;
+        y = LOWEST_Y;
+
 
 
 
     }
+    //移动
+    public void move(){
+        step();
+        if ( y >= LOWEST_Y){
+            jumpValue = -4;
+        }
+        if ( y <= LOWEST_Y){
+            jumpValue = 4;
+        }
+        y = y +jumpValue;
+        if (y >= LOWEST_Y){
+            jumpState = false;
+        }
 
+
+
+    }
+    //踏步
+    private void step() {
+        int tmp = stepTime / 100 % 2;
+        if (tmp == 1){
+            image = image1;
+        }else {
+            image = image2;
+        }
+        stepTime = stepTime +  fresh;
+    }
+
+    //跳跃
+    public void jump(){
+        jumpState = true;
+    }
+    //接触死亡区域
+
+    public Rectangle bound1(){
+        return new Rectangle(x+20,y,20,10);
+    }
+
+    public Rectangle bound2(){
+        return new Rectangle(x+5,y+35,20,10);
+    }
 
 
 
